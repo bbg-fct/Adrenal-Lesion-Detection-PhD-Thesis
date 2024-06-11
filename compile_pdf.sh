38dd26a26c8fb278d0fabfab01cb4a7303c72175
Bernardo Goncalves <bb.goncalves@campus.fct.unl.pt>
2024-06-11 21:05:13 +0100

    adds sh to compile pdf

--- compile_pdf.sh
+++ compile_pdf.sh
@@ -1,2 +1,2 @@
 #! bin/bash
-latexmk -shell-escape -file-line-error -pdf BernardoGoncalvesPhDThesis
+latexmk -shell-escape -file-line-error -pdf -outdir=. -auxdir=aux_files BernardoGoncalvesPhDThesis
